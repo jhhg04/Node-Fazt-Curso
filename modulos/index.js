3c8917e0f701c1f4275ff1d1a08a449ec1c1c667
jhhg04 <jhhg04@gmail.com>
2021-11-04 22:44:21 -0500

    first commit

---
+++ modulos/index.js
@@ -0,0 +1,10 @@
+const os = require('os');
+const fs = require('fs');
+
+/* 
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.release());
+console.log(`Free mem: ${os.freemem()} bytes`);
+console.log('Free mem:', os.freemem(), ' bytes');
+*/
