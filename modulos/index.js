6efa0922aa37ddcfc092f7d9916f0f65d55ebdc6
jhhg04 <jhhg04@gmail.com>
2021-11-05 21:41:55 -0500

    add

--- modulos/index.js
+++ modulos/index.js
@@ -1,7 +1,58 @@
 const os = require('os');
 const fs = require('fs');
+const http = require('http');
+const colors = require('colors');
+const express = require('express');
+
+const server = express();
+
+server.listen(3000, () => {
+  console.log('Server on port 3000'.green);
+});
+
+server.get('/', (req, res) => {
+  res.send('<h1>Hello World from Express and Node</h1>');
+  res.end();
+});
+
+/*
+// Server Node
+const handleServer = function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write('<h1>Hello World</h1>');
+    res.end();
+};
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function () {
+    console.log('Server on port 3000'.green);
+});
+*/
 
 /* 
+// Leer archivo que se creo
+fs.readFile('./texto.txt', function (err, data) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(data.toString());
+});
+*/
+
+/* 
+// Crear archivo
+fs.writeFile('./texto.txt', 'linea uno', function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Archivo Creado');
+});
+console.log('ultima linea de codigo');
+*/
+
+/* 
+// Use metodos del sistema
 console.log(os.hostname());
 console.log(os.platform());
 console.log(os.release());
